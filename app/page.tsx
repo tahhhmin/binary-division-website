a40ba9a2f28113b31b56fbeb545c3b55e74a64a0
tahhhmin <tajriantahmin@gmail.com>
2025-11-30 14:50:29 +0600

    changes changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import styles from './styles/page.module.css'
 import { Button } from "@/components/ui/button"
 import { ArrowUpIcon } from "lucide-react"
 
 export default function page() {
     return (
-        <div>
+        <main className={styles.main}>
             <Button variant="outline">Button</Button>
             <Button variant="outline" size="icon" aria-label="Submit">
                 <ArrowUpIcon />
             </Button>
-        </div>
+        </main>
     )
 }
